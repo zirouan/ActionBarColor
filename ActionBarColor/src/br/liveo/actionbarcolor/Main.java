d9af771d3caca746d3705c793e0a1210a5cc8a8a
Rudson Lima <rudsonlive@gmail.com>
2014-05-06 15:30:47 -0300

    Projeto ActionBarColor

---
+++ ActionBarColor/src/br/liveo/actionbarcolor/Main.java
@@ -0,0 +1,14 @@
+package br.liveo.actionbarcolor;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class Main extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
